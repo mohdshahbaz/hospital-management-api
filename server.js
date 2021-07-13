320c42dc7d173255707fdcafd4acbb49337c17f1
shahbaz <mohdshahbaz@gmail.com>
2021-07-13 15:57:54 +0530

    fixed cors error

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ app.listen(port, () => {
 console.log(`Server running at http://localhost:${port}`);
 });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+})
+
 // Import Db connectyion
 require("./config/db");
 
